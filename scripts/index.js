22ad7f34f60c1389a4ed73c9cb4fb231e5929378
Akloib <ali.k0beissi.ak@gmail.com>
2021-11-16 07:36:06 +0100

    inital commit

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+// shrink header after scrolling down
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+    var navs = document.getElementsByClassName("nav-link");
+
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+
+        document.getElementsByClassName("logo-size")[0].style.width = "100px";
+
+        // document.getElementsByClassName("logo-size")[0].style.maxHeight = "80px";
+
+        document.getElementsByClassName("header-container")[0].style.maxHeight = "3rem";
+
+        for(let i=0; i<navs.length; i++) {
+            navs[i].style.fontSize = "1rem";
+        }
+
+    } else {
+
+        document.getElementsByClassName("logo-size")[0].style.width = "150px";
+
+        // document.getElementsByClassName("logo-size")[0].style.maxHeight = "100px";
+
+        document.getElementsByClassName("header-container")[0].style.maxHeight = "4rem";
+
+        for(let i=0; i<navs.length; i++) {
+            navs[i].style.fontSize = "1.25rem";
+        }
+
+    }
+}
